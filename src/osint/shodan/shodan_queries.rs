9184514cc774e12d0175d2ece5ea8f6d98312914
lambert <leandro.salustriano@hotmail.com>
2025-12-11 18:52:20 -0300

    Remove `shodan_basic_filter` from React exploit and standardize Shodan query filter formatting
    - Delete `shodan_basic_filter` method from `react_unsafely_deserialize_rce.rs` as it duplicates existing functionality in `shodan_queries.rs`.
    - Standardize string formatting for all Shodan query filters across `shodan_queries.rs`.

--- src/osint/shodan/shodan_queries.rs
+++ src/osint/shodan/shodan_queries.rs
@@ -1,140 +1,140 @@
 pub fn shodan_rsdw_webpack() -> Vec<String> {
     vec![
-        r#"http.html:"react-server-dom-webpack""#.to_string(),
-        r#"http.component:"react-server-dom-webpack""#.to_string(),
-        r#"product:"react-server-dom-webpack""#.to_string(),
-        r#"http.html:"react flight""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
-        r#"http.html:"react-server-dom""#.to_string(),
-        r#"http.html:"use server""#.to_string(),
-        r#"http.html:"use client""#.to_string(),
+        r#""http.html:"react-server-dom-webpack""#.to_string(),
+        r#""http.component:"react-server-dom-webpack""#.to_string(),
+        r#""product:"react-server-dom-webpack""#.to_string(),
+        r#""http.html:"react flight""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
+        r#""http.html:"react-server-dom""#.to_string(),
+        r#""http.html:"use server""#.to_string(),
+        r#""http.html:"use client""#.to_string(),
     ]
 }
 
 pub fn shodan_rsd_parcel() -> Vec<String> {
     vec![
-        r#"http.html:"react-server-dom-parcel""#.to_string(),
-        r#"http.component:"react-server-dom-parcel""#.to_string(),
-        r#"product:"react-server-dom-parcel""#.to_string(),
-        r#"http.html:"@parcel/rsc""#.to_string(),
-        r#"http.html:"parcel rsc""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
-        r#"http.html:"react flight""#.to_string(),
+        r#""http.html:"react-server-dom-parcel""#.to_string(),
+        r#""http.component:"react-server-dom-parcel""#.to_string(),
+        r#""product:"react-server-dom-parcel""#.to_string(),
+        r#""http.html:"@parcel/rsc""#.to_string(),
+        r#""http.html:"parcel rsc""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
+        r#""http.html:"react flight""#.to_string(),
     ]
 }
 
 pub fn shodan_rsd_turbopack() -> Vec<String> {
     vec![
-        r#"http.html:"react-server-dom-turbopack""#.to_string(),
-        r#"http.component:"react-server-dom-turbopack""#.to_string(),
-        r#"product:"react-server-dom-turbopack""#.to_string(),
-        r#"http.html:"turbopack""#.to_string(),
-        r#"http.html:"next-flight""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
+        r#""http.html:"react-server-dom-turbopack""#.to_string(),
+        r#""http.component:"react-server-dom-turbopack""#.to_string(),
+        r#""product:"react-server-dom-turbopack""#.to_string(),
+        r#""http.html:"turbopack""#.to_string(),
+        r#""http.html:"next-flight""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
     ]
 }
 
 pub fn shodan_nextjs() -> Vec<String> {
     vec![
-        r#"http.component:"Next.js""#.to_string(),
-        r#"product:"Next.js""#.to_string(),
-        r#"title:"Next.js""#.to_string(),
-        r#"http.component:"_next""#.to_string(),
-        r#"http.html:"/_next""#.to_string(),
-        r#"http.html:"/_next/static""#.to_string(),
-        r#"http.html:"/_next/data""#.to_string(),
-        r#"http.html:"/_next/image""#.to_string(),
-        r#"http.html:"__NEXT_DATA__""#.to_string(),
-        r#"http.html:"window.__NEXT_DATA__""#.to_string(),
-        r#"http.headers.x-powered-by:"Next.js""#.to_string(),
-        r#"http.headers.server:"Next.js""#.to_string(),
-        r#"http.title:"This page could not be found""#.to_string(),
-        r#"http.html:"This page could not be found""#.to_string(),
-        r#"http.favicon.hash:-1424036600"#.to_string(),
-        r#"http.html:"next/font""#.to_string(),
-        r#"http.html:"next/script""#.to_string(),
-        r#"http.html:"/_next/static/media""#.to_string(),
-        r#"http.html:"/_next/static/runtime""#.to_string(),
-        r#"http.html:"use server""#.to_string(),
-        r#"http.html:"use client""#.to_string(),
-        r#"http.html:"react-server-dom-webpack""#.to_string(),
+        r#""http.component:"Next.js""#.to_string(),
+        r#""product:"Next.js""#.to_string(),
+        r#""title:"Next.js""#.to_string(),
+        r#""http.component:"_next""#.to_string(),
+        r#""http.html:"/_next""#.to_string(),
+        r#""http.html:"/_next/static""#.to_string(),
+        r#""http.html:"/_next/data""#.to_string(),
+        r#""http.html:"/_next/image""#.to_string(),
+        r#""http.html:"__NEXT_DATA__""#.to_string(),
+        r#""http.html:"window.__NEXT_DATA__""#.to_string(),
+        r#""http.headers.x-powered-by:"Next.js""#.to_string(),
+        r#""http.headers.server:"Next.js""#.to_string(),
+        r#""http.title:"This page could not be found""#.to_string(),
+        r#""http.html:"This page could not be found""#.to_string(),
+        r#""http.favicon.hash:-1424036600"#.to_string(),
+        r#""http.html:"next/font""#.to_string(),
+        r#""http.html:"next/script""#.to_string(),
+        r#""http.html:"/_next/static/media""#.to_string(),
+        r#""http.html:"/_next/static/runtime""#.to_string(),
+        r#""http.html:"use server""#.to_string(),
+        r#""http.html:"use client""#.to_string(),
+        r#""http.html:"react-server-dom-webpack""#.to_string(),
     ]
 }
 
 pub fn shodan_react_router_rsc() -> Vec<String> {
     vec![
-        r#"http.html:"react-router""#.to_string(),
-        r#"http.component:"react-router""#.to_string(),
-        r#"product:"react-router""#.to_string(),
-        r#"http.html:"react-router/server""#.to_string(),
-        r#"http.html:"react-router rsc""#.to_string(),
-        r#"http.html:"use server""#.to_string(),
+        r#""http.html:"react-router""#.to_string(),
+        r#""http.component:"react-router""#.to_string(),
+        r#""product:"react-router""#.to_string(),
+        r#""http.html:"react-router/server""#.to_string(),
+        r#""http.html:"react-router rsc""#.to_string(),
+        r#""http.html:"use server""#.to_string(),
     ]
 }
 
 pub fn shodan_waku() -> Vec<String> {
     vec![
-        r#"http.html:"@waku/react-server""#.to_string(),
-        r#"http.html:"waku rsc""#.to_string(),
-        r#"http.component:"waku""#.to_string(),
-        r#"product:"waku""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
+        r#""http.html:"@waku/react-server""#.to_string(),
+        r#""http.html:"waku rsc""#.to_string(),
+        r#""http.component:"waku""#.to_string(),
+        r#""product:"waku""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
     ]
 }
 
 pub fn shodan_redwoodjs() -> Vec<String> {
     vec![
-        r#"http.html:"redwoodjs""#.to_string(),
-        r#"http.html:"redwood rsc""#.to_string(),
-        r#"http.html:"@redwoodjs/router""#.to_string(),
-        r#"product:"redwoodjs""#.to_string(),
-        r#"http.component:"redwood""#.to_string(),
+        r#""http.html:"redwoodjs""#.to_string(),
+        r#""http.html:"redwood rsc""#.to_string(),
+        r#""http.html:"@redwoodjs/router""#.to_string(),
+        r#""product:"redwoodjs""#.to_string(),
+        r#""http.component:"redwood""#.to_string(),
     ]
 }
 
 pub fn shodan_vite_rsc() -> Vec<String> {
     vec![
-        r#"http.html:"vite""#.to_string(),
-        r#"http.component:"vite""#.to_string(),
-        r#"product:"vite""#.to_string(),
-        r#"http.html:"@vitejs/plugin-rsc""#.to_string(),
-        r#"http.html:"vite rsc""#.to_string(),
-        r#"http.html:"use server""#.to_string(),
+        r#""http.html:"vite""#.to_string(),
+        r#""http.component:"vite""#.to_string(),
+        r#""product:"vite""#.to_string(),
+        r#""http.html:"@vitejs/plugin-rsc""#.to_string(),
+        r#""http.html:"vite rsc""#.to_string(),
+        r#""http.html:"use server""#.to_string(),
     ]
 }
 
 pub fn shodan_parcel_rsc() -> Vec<String> {
     vec![
-        r#"http.html:"parcel""#.to_string(),
-        r#"product:"parcel""#.to_string(),
-        r#"http.component:"parcel""#.to_string(),
-        r#"http.html:"@parcel/rsc""#.to_string(),
-        r#"http.html:"parcel rsc""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
+        r#""http.html:"parcel""#.to_string(),
+        r#""product:"parcel""#.to_string(),
+        r#""http.component:"parcel""#.to_string(),
+        r#""http.html:"@parcel/rsc""#.to_string(),
+        r#""http.html:"parcel rsc""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
     ]
 }
 
 pub fn shodan_webpack_rsc() -> Vec<String> {
     vec![
-        r#"http.html:"webpack""#.to_string(),
-        r#"http.component:"webpack""#.to_string(),
-        r#"product:"webpack""#.to_string(),
-        r#"http.html:"react-server-dom-webpack""#.to_string(),
-        r#"http.html:"react-server""#.to_string(),
+        r#""http.html:"webpack""#.to_string(),
+        r#""http.component:"webpack""#.to_string(),
+        r#""product:"webpack""#.to_string(),
+        r#""http.html:"react-server-dom-webpack""#.to_string(),
+        r#""http.html:"react-server""#.to_string(),
     ]
 }
 
 pub fn shodan_turbopack() -> Vec<String> {
     vec![
-        r#"http.html:"turbopack""#.to_string(),
-        r#"http.component:"turbopack""#.to_string(),
-        r#"product:"turbopack""#.to_string(),
-        r#"http.html:"next-flight""#.to_string(),
+        r#""http.html:"turbopack""#.to_string(),
+        r#""http.component:"turbopack""#.to_string(),
+        r#""product:"turbopack""#.to_string(),
+        r#""http.html:"next-flight""#.to_string(),
     ]
 }
 pub fn shodan_expo_rsc() -> Vec<String> {
     vec![
-        r#"http.html:"expo""#.to_string(),
+        r#""http.html:"expo""#.to_string(),
         r#"http.html:"expo rsc""#.to_string(),
         r#"http.html:"react-server""#.to_string(),
     ]
@@ -265,7 +265,7 @@ pub fn shodan_custom_rsc() -> Vec<String> {
 
 pub fn applications_by_title() -> Vec<String> {
     vec![
-        r#"product":"Portainer""#.to_string(),
+        r#""product":"Portainer""#.to_string(),
         r#"product":"Backoffice""#.to_string(),
         r#"product":"Tower360 Suite""#.to_string(),
         r#"product":"Wabot.app""#.to_string(),
